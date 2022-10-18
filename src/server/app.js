e3bf4d3b65ecac58597d41803a1d8067a2eea468
Matt Mayer <mayerm94@gmail.com>
2022-10-18 18:09:09 -0400

    Uncomment cors middleware

--- src/server/app.js
+++ src/server/app.js
@@ -26,7 +26,7 @@ const createApp = ({ corsConfig }) => {
   app.use('/fail', fail);
 
   // Apply CORS to the endpoints
-  //app.use(cors(corsConfig));
+  app.use(cors(corsConfig));
 
   // Add body parser
   app.use(bodyParser.json());
