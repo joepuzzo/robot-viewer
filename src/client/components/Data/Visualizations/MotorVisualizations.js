9d7bb6946ab5e7ec66f543d129c0ebe694c99cbf
joepuzzo <jgpuzzo2@gmail.com>
2022-11-07 08:21:31 -0800

    added format to graph label and start and stop capture

--- src/client/components/Data/Visualizations/MotorVisualizations.js
+++ src/client/components/Data/Visualizations/MotorVisualizations.js
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useFieldState, useFormApi } from 'informed';
 
-import { Flex } from '@adobe/react-spectrum';
+import { Button, Flex } from '@adobe/react-spectrum';
 import LineGraph from './LineGraph';
 
 import useRobotState from '../../../hooks/useRobotState';
 import useApp from '../../../hooks/useApp';
+import { useStateWithGetter } from '../../../hooks/useStateWithGetter';
 
 export const MotorVisualizations = () => {
-
   // EXAMPLE IGUS
   const exampleIgus = {
     id: 'j0',
@@ -42,7 +42,13 @@ export const MotorVisualizations = () => {
     sendInterval: 20,
     calculatedVelocity: 29,
     currentVelocity: 30,
-    positionHistory: [{time: 1, position:10}, {time: 2, position:20},{time: 3, position:30},{time: 4, position:30},{time: 5, position:50},]
+    positionHistory: [
+      { time: 1, position: 10 },
+      { time: 2, position: 20 },
+      { time: 3, position: 30 },
+      { time: 4, position: 30 },
+      { time: 5, position: 50 },
+    ],
   };
 
   const { socket } = useApp();
@@ -54,6 +60,8 @@ export const MotorVisualizations = () => {
   const robotState = robotStates[robotId];
   const selectedMotor = robotState?.[motorId] || exampleIgus;
   const formApi = useFormApi();
+  0;
+  const [capture, setCapture, getCapture] = useStateWithGetter(true);
 
   // Use me for fake position updates
   // useEffect(() => {
@@ -64,38 +72,40 @@ export const MotorVisualizations = () => {
   //   return () => clearInterval(interval);
   // }, []);
   //const [positionData, setPositionData] = useState(selectedMotor.positionHistory.map((i) => ({ x: i.time, y: i.position }))  );
-  
 
-  const [positionData, setPositionData] = useState([{x:Date.now(), y: 0}]);
-  const [velocityData, setVelocityData] = useState([{x:Date.now(), y: 0}]);
-
+  const [positionData, setPositionData] = useState([{ x: Date.now(), y: 0 }]);
+  const [velocityData, setVelocityData] = useState([{ x: Date.now(), y: 0 }]);
 
   const stateHandler = (id, robotState) => {
     const motorId = formApi.getFormState().values?.motorId;
-    if(motorId){
+    if (motorId && getCapture()) {
       setPositionData((prev) => {
-        if(prev.length >= 500){
+        if (prev.length >= 500) {
           prev.shift();
         }
-        prev.push({x: Date.now(), y: robotState?.motors?.[motorId]?.currentPosition || 0})
+        prev.push({ x: Date.now(), y: robotState?.motors?.[motorId]?.currentPosition || 0 });
         return [...prev];
-      })
+      });
       setVelocityData((prev) => {
-        if(prev.length >= 500){
+        if (prev.length >= 500) {
           prev.shift();
         }
-        prev.push({x: Date.now(), y: robotState?.motors?.[motorId]?.currentVelocity || 0})
+        prev.push({ x: Date.now(), y: robotState?.motors?.[motorId]?.currentVelocity || 0 });
         return [...prev];
-      })
+      });
     }
   };
 
-
   useEffect(() => {
-   socket.on('robot', stateHandler);
-   return () => {socket.removeListener('robot', stateHandler)};
-  },[]);
+    socket.on('robot', stateHandler);
+    return () => {
+      socket.removeListener('robot', stateHandler);
+    };
+  }, []);
 
+  const toggleCapture = useCallback(() => {
+    setCapture((prev) => !prev);
+  }, []);
 
   return (
     <Flex
@@ -105,10 +115,24 @@ export const MotorVisualizations = () => {
       alignItems="center"
       gap="size-100"
     >
+      <h2>Capture</h2>
+      <Button onPress={toggleCapture}>{capture ? 'Stop' : 'Start'}</Button>
       <h2>Position</h2>
-      <LineGraph data={positionData} xMin={positionData[0]?.x || Date.now()} xMax={positionData[positionData.length -1]?.x || Date.now()} yMin={0} yMax={360} />
+      <LineGraph
+        data={positionData}
+        xMin={positionData[0]?.x || Date.now()}
+        xMax={positionData[positionData.length - 1]?.x || Date.now()}
+        yMin={0}
+        yMax={360}
+      />
       <h2>Velocity</h2>
-      <LineGraph data={velocityData} xMin={velocityData[0]?.x || Date.now()} xMax={velocityData[velocityData.length -1]?.x || Date.now()} yMin={0} yMax={100} />
+      <LineGraph
+        data={velocityData}
+        xMin={velocityData[0]?.x || Date.now()}
+        xMax={velocityData[velocityData.length - 1]?.x || Date.now()}
+        yMin={0}
+        yMax={100}
+      />
     </Flex>
   );
 };
